6cf5fdb4b13393f7f2ecf9ff34bf240b1a6276a0
Francoveliz <francoleandroveliz@gmail.com>
2021-08-29 02:58:23 -0300

    added searchbar feature

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -5,16 +5,22 @@ import { Container, Box } from "@chakra-ui/react";
 import ProjectsTable from "./ProjectsTable";
 
 const ProjectsList = () => {
+	const searchText = useSelector(state => state.search.searchText);
 	const projects = useSelector(state => state.projects.projects);
+
+	const filterProjects = projects.filter(project =>
+		project.projectName.toLowerCase().includes(searchText.toLowerCase())
+	);
+
 	return (
 		<div>
 			<Container maxW="container.xl" px={{ base: 0, md: 4 }}>
 				<Box display={{ md: "none", base: "block" }}>
-					{projects.map(project => (
+					{filterProjects.map(project => (
 						<ProjectCard {...project} key={project.id} />
 					))}
 				</Box>
-				<ProjectsTable projects={projects} />
+				<ProjectsTable projects={filterProjects} />
 			</Container>
 		</div>
 	);
